0ecd137ca18e1e2816dc467f1bf1b8e521fb977d
Kateryna Zhyrnova <kateryna.zhyrnova.sparkle@gmail.com>
2024-08-02 12:39:42 +0200

    add task array distructuring part 2

--- array-destructuring/start.js
+++ array-destructuring/start.js
@@ -1,4 +1,4 @@
-// TASK 1
+// TASK 1 ----------------------------------------------------------------------------------------------------
 
 /*
 Modify parameters section in the "processQuantities" function to match console.log outputs
@@ -14,3 +14,22 @@ const processQuantities = (/* parameters */) => {
 const qtyRange = [8, 29];
 
 processQuantities(qtyRange);
+
+// TASK 2 ----------------------------------------------------------------------------------------------------
+
+/*
+Create a function "minMax" that will accept any quantity of the arguments.
+
+This function should return array of two elements:
+1. First element in the array is minimal value among all arguments
+2. Second element in the array is maximal value among all arguments
+
+Use destructuring to parse results of the function calls.
+*/
+
+let min, max;
+/* call here "minMax" function with arguments 24, 5, 34, 10 */
+console.log(min, max); // 5, 34
+
+/* call here "minMax" function with arguments 18, 23, 103, 70, 80, 25 */
+console.log(min, max); // 18, 103
